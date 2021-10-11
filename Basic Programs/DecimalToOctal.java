d1cd53c016a27c8c7173a22c5484232aaa5e34fd
shravanatirtha <shravz.ceo@gmail.com>
2021-10-11 14:18:09 +0530

    added DecimalToOctal program

---
+++ Basic Programs/DecimalToOctal.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class DecimalToOctal {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int decimal=sc.nextInt();
+        int rem;  
+        String hex=""; 
+        char hexchars[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};  
+        while(decimal>0)  
+     {  
+       rem=decimal%16;   
+       hex=hexchars[rem]+hex;   
+       decimal=decimal/16;  
+     }  
+        System.out.println(hex);
+    }
+}
