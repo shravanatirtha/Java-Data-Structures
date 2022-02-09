ab64944053aa283dd70f1528fa0ecaee3aab9057
shravanatirtha <shravz.ceo@gmail.com>
2022-02-09 15:26:37 +0530

    formatted

--- Basic-Programs/DecimalToOctal.java
+++ Basic-Programs/DecimalToOctal.java
@@ -1,21 +1,19 @@
-import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class DecimalToOctal {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int decimal=sc.nextInt();
-        int rem;  
-     String octal=""; 
-        char octalchars[]={'0','1','2','3','4','5','6','7'};  
-     
-    while(decimal>0)  
-    {  
-       rem=decimal%8;   
-       octal=octalchars[rem]+octal;   
-       decimal=decimal/8;  
-    }  
+        Scanner sc = new Scanner(System.in);
+        int decimal = sc.nextInt();
+        int rem;
+        String octal = "";
+        char octalchars[] = { '0', '1', '2', '3', '4', '5', '6', '7' };
+
+        while (decimal > 0) {
+            rem = decimal % 8;
+            octal = octalchars[rem] + octal;
+            decimal = decimal / 8;
+        }
         System.out.println(octal);
     }
 }
