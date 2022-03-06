df63a9f6239099154a13a19e3c2f01ae2a4b9244
shravanatirtha <shravz.ceo@gmail.com>
2022-03-06 21:16:13 +0530

    added leetcode hard

---
+++ Leetcode Hard/countOrders.java
@@ -0,0 +1,17 @@
+/*
+Given n orders, each order consist in pickup and delivery services. 
+
+Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i). 
+
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+*/  
+public class countOrders {
+    public int countOrders(int n) {
+        long res = 1;
+        long mod = (long)1e9 + 7;
+       for (int i = 1; i <= n; ++i)
+           res = res * (i * 2 - 1) * i % mod;
+       return (int)res;
+   }
+}
--- MaxWinScore.java
+++ MaxWinScore.java
@@ -25,7 +25,7 @@ class Node {
     Node right;
     Node left;
 
-    Nonde(int data){
+    Node(int data){
         this.data=data;
         left=null;
         right=null;
