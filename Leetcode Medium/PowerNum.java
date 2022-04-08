d1d696c5634c139a0e9e0028d43bc402fa6c24d9
shravanatirtha <shravz.ceo@gmail.com>
2022-04-08 16:54:57 +0530

    modified

--- Leetcode Medium/PowerNum.java
+++ Leetcode Medium/PowerNum.java
@@ -5,14 +5,31 @@ public class PowerNum {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(solve(n, m));
+        System.out.println(solve1(n, m));
+        System.out.println(solve2(n, m));
         sc.close();
     }
 
-    static int solve(int n,int m) {
-        if(m==1 || m==0) return 1;
-        else{
-            return n*solve(n,m-1);
-        }
+    // regular method
+    static int solve1(int n, int m) {
+        if (m == 0)
+            return 1; // base cases
+        if (m == 1)
+            return n;
+        else
+            return n * solve1(n, m - 1);// recursive case
+
+    }
+
+    // decomposition
+    static int solve2(int n, int m) {
+        if (m == 0)
+            return 1; // base cases
+        if (m == 1)
+            return n;
+        if (m % 2 == 0)
+            return solve2(n, m / 2) * n;
+        else
+            return solve2(n * n, m / 2) * n;
     }
 }
