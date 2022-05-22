3722717ab4fac150d730d0eeedb097885043b8d3
shravanatirtha <shravz.ceo@gmail.com>
2022-05-22 19:07:37 +0530

    added heap

--- Leetcode Easy/HeapSort.java
+++ Leetcode Easy/HeapSort.java
@@ -1,15 +1,27 @@
 import java.util.*;
+
 public class HeapSort {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] list=new int[n];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++)
+            list[i] = sc.nextInt();
         sc.close();
-        heapsort(list, n);
+        Heap h = new Heap();
+        h.heapsort(list, n);
+
     }
-    static void heapsort(int[] list, int n){
-        PriorityQueue<Integer> heap=new PriorityQueue<>();
+
+}
+
+class Heap {
+    void heapsort(int[] list, int n) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            heap.add(list[i]);
+        }
+        System.out.print(heap.toString().replace(",", "").replace("[", "").replace("]", ""));
 
     }
 }
-
